910429a5a448a66cd987fd2c04c672a641f33326
bryguy82 <and18017@byui.edu>
2019-09-16 19:32:53 -0300

    Initial commit

---
+++ CIT365/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CIT365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name?");
+            var username = Console.ReadLine();
+            Console.WriteLine($"\nHello {username}!");
+            int dayOfYear = DateTime.Now.DayOfYear;
+
+            Console.Write("Day of year: ");
+            Console.WriteLine(dayOfYear);
+        }
+    }
+}
